7c0a5252653c211412579c0e5859044419efe214
SamiChafik <sami.chafik00@gmail.com>
2025-06-06 21:06:19 +0100

    cors

--- src/main/java/com/example/HelloEvents/security/SecurityConfig.java
+++ src/main/java/com/example/HelloEvents/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.HelloEvents.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/event/allEvents").permitAll() // Allow viewing events without authentication
 
                         // Client endpoints
-                        .requestMatchers("/user/{id}").hasAnyRole("CLIENT", "ADMIN") // View profile
+//                        .requestMatchers("/user/{id}").hasAnyRole("CLIENT") // View profile
                         .requestMatchers("/reservation/**").hasRole("CLIENT") // All reservation operations
 
                         // Admin endpoints
@@ -44,6 +45,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // JWT doesn't need sessions
                 )
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);//search about it
 
         return http.build();
