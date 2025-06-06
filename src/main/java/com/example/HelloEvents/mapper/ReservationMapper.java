758df4c6b4d3465b3674cc9026be5f65826b5c2e
SamiChafik <sami.chafik00@gmail.com>
2025-06-06 21:05:14 +0100

    var rename

--- src/main/java/com/example/HelloEvents/mapper/ReservationMapper.java
+++ src/main/java/com/example/HelloEvents/mapper/ReservationMapper.java
@@ -9,9 +9,9 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring" , unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface ReservationMapper {
 
-    @Mapping(source = "reservation.idreservation", target = "idReservation")
-    @Mapping(source = "reservation.user.id", target = "userId")
-    @Mapping(source = "reservation.event.idEvent", target = "eventId")
+    @Mapping(source = "reservation.reservation_id", target = "reservation_id")
+    @Mapping(source = "reservation.user.id", target = "user_id")
+    @Mapping(source = "reservation.event.event_id", target = "event_id")
     ReservationDto reservationToDto(Reservation reservation);
 
 
